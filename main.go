1e38b279cb62bc855a25ed29bcc260475ca71309
zohan <zohan@ayagroup.pro>
2022-12-14 00:59:02 +0300

    Добавил получение урлов из постман

--- main.go
+++ main.go
@@ -35,10 +35,10 @@ type RequestData struct {
 }
 
 type Collections struct {
-	Collections []Collection
+	Collections []CollectionFromCollections
 }
 
-type Collection struct {
+type CollectionFromCollections struct {
 	Id        string    `json:"id"`
 	Name      string    `json:"name"`
 	Owner     string    `json:"owner"`
@@ -53,26 +53,91 @@ type Collection struct {
 	IsPublic bool `json:"isPublic"`
 }
 
+type CollectionResponseObject struct {
+	Collection Collection `json:"collection"`
+}
+
+type Collection struct {
+	Info struct {
+		PostmanId   string `json:"_postman_id"`
+		Name        string `json:"name"`
+		Description string `json:"description"`
+		Schema      string `json:"schema"`
+		Fork        struct {
+			Label     string    `json:"label"`
+			CreatedAt time.Time `json:"createdAt"`
+			From      string    `json:"from"`
+		} `json:"fork"`
+		UpdatedAt time.Time `json:"updatedAt"`
+	}
+	Item []CollectionItem
+}
+
+type CollectionItem struct {
+	Name                    string           `json:"name"`
+	Item                    []CollectionItem `json:"item"`
+	Id                      string           `json:"id"`
+	ProtocolProfileBehavior struct {
+		DisableBodyPruning bool `json:"disableBodyPruning"`
+	} `json:"protocolProfileBehavior"`
+	Request struct {
+		Method string        `json:"method"`
+		Header []interface{} `json:"header"`
+		Url    struct {
+			Raw  string   `json:"raw"`
+			Host []string `json:"host"`
+			Path []string `json:"path"`
+		} `json:"url"`
+	} `json:"request"`
+	Response []interface{} `json:"response"`
+}
+
 func main() {
 	//Добавить валидацию, что Env задана
 	workspaceId := getWorkspaceId()
 	collectionId := getCollectionId(workspaceId)
+	_ = getUrlsFromPostman(collectionId)
 	fmt.Println("Workspace Id: \t", workspaceId)
-	fmt.Println("Collection Id: \t", collectionId)
+	fmt.Println("CollectionFromCollections Id: \t", collectionId)
+}
+
+func getUrlsFromPostman(collectionId string) int {
+	url := "https://api.getpostman.com/collections/" + collectionId
+	requestData := RequestData{}
+	data := getResponse(url, "GET", requestData)
+	collectionResponseObject := &CollectionResponseObject{}
+	err := json.Unmarshal(data, collectionResponseObject)
+	if err != nil {
+		panic(err)
+	}
+
+	// TODO переделать на slice который после можно будет сравнивать с выводом получаемым от php artisan route:list
+	printRequestUrlRaw(collectionResponseObject.Collection.Item)
+
+	return 0
+}
+
+func printRequestUrlRaw(items []CollectionItem) {
+	for _, item := range items {
+		if item.Item != nil {
+			printRequestUrlRaw(item.Item)
+		}
+		fmt.Println(item.Request.Url.Raw)
+	}
 }
 
 func getCollectionId(workspaceId string) string {
 	collections := getCollections(workspaceId)
 	for _, collection := range collections {
 		if collection.Fork.Label == "staging" {
-			return collection.Id
+			return collection.Uid
 		}
 	}
 
 	panic(errors.New("collection not found"))
 }
 
-func getCollections(workspaceId string) []Collection {
+func getCollections(workspaceId string) []CollectionFromCollections {
 	url := "https://api.getpostman.com/collections"
 	queryParams := []QueryParams{
 		{Key: "workspace", Value: workspaceId},
