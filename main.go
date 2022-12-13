294b60129c16f73631c4ba6e7b039147e2fa0fa3
zohan <zohan@ayagroup.pro>
2022-12-13 23:56:20 +0300

    Добавил получение collection id

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 type Workspaces struct {
@@ -23,9 +24,71 @@ type Workspace struct {
 	Visibility string `json:"visibility"`
 }
 
+type QueryParams struct {
+	Key   string
+	Value string
+}
+
+type RequestData struct {
+	Body        io.Reader
+	QueryParams []QueryParams
+}
+
+type Collections struct {
+	Collections []Collection
+}
+
+type Collection struct {
+	Id        string    `json:"id"`
+	Name      string    `json:"name"`
+	Owner     string    `json:"owner"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	Uid       string    `json:"uid"`
+	Fork      struct {
+		Label     string    `json:"label"`
+		CreatedAt time.Time `json:"createdAt"`
+		From      string    `json:"from"`
+	} `json:"fork"`
+	IsPublic bool `json:"isPublic"`
+}
+
 func main() {
+	//Добавить валидацию, что Env задана
 	workspaceId := getWorkspaceId()
+	collectionId := getCollectionId(workspaceId)
 	fmt.Println("Workspace Id: \t", workspaceId)
+	fmt.Println("Collection Id: \t", collectionId)
+}
+
+func getCollectionId(workspaceId string) string {
+	collections := getCollections(workspaceId)
+	for _, collection := range collections {
+		if collection.Fork.Label == "staging" {
+			return collection.Id
+		}
+	}
+
+	panic(errors.New("collection not found"))
+}
+
+func getCollections(workspaceId string) []Collection {
+	url := "https://api.getpostman.com/collections"
+	queryParams := []QueryParams{
+		{Key: "workspace", Value: workspaceId},
+	}
+	requestData := RequestData{
+		QueryParams: queryParams,
+	}
+	data := getResponse(url, "GET", requestData)
+	collections := &Collections{}
+	err := json.Unmarshal(data, collections)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return collections.Collections
 }
 
 func getWorkspaceId() string {
@@ -37,12 +100,13 @@ func getWorkspaceId() string {
 		}
 	}
 
-	panic(errors.New("empty name"))
+	panic(errors.New("workspace not found"))
 }
 
 func getWorkspaces() []Workspace {
 	url := "https://api.getpostman.com/workspaces"
-	data := getResponse(url, "GET", nil)
+	requestData := RequestData{}
+	data := getResponse(url, "GET", requestData)
 	workspacesApi := &Workspaces{}
 	err := json.Unmarshal(data, workspacesApi)
 
@@ -53,13 +117,24 @@ func getWorkspaces() []Workspace {
 	return workspacesApi.Workspaces
 }
 
-func getResponse(url string, method string, body io.Reader) []byte {
+func getResponse(url string, method string, requestData RequestData) []byte {
 	apiKey := os.Getenv("API_KEY")
 	headerKey := "X-API-Key"
 
 	client := &http.Client{}
+	body := requestData.Body
+	queryParams := requestData.QueryParams
 	req, _ := http.NewRequest(method, url, body)
 	req.Header.Set(headerKey, apiKey)
+
+	q := req.URL.Query()
+	if queryParams != nil {
+		for _, queryParam := range queryParams {
+			q.Add(queryParam.Key, queryParam.Value)
+		}
+	}
+	req.URL.RawQuery = q.Encode()
+
 	resp, err := client.Do(req)
 
 	if err != nil {
